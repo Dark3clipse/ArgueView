0776076d37d3fab4d77b12ef94e5138fdd3f5030
SophiaHadash <sophiahadash@gmail.com>
2021-01-13 11:55:37 +0100

    optional interactivity

--- argueview/visualizers/FeatureListVisualizer.py
+++ argueview/visualizers/FeatureListVisualizer.py
@@ -4,15 +4,15 @@ from argueview.typings import Explanation, LatentContinuousTargetDisplay, Framin
 from argueview.visualizers import initjs, html_err_msg
 
 
-def FeatureListVisualizer(explanation: Explanation, framing: Framing = "positive", lct: LatentContinuousTargetDisplay = "label", visualization: FeatureListVisualizationType = "badge", threshold: float = -2, threshold_badge: float = 0, threshold_omit: float = -1) -> None:
+def FeatureListVisualizer(explanation: Explanation, framing: Framing = "positive", lct: LatentContinuousTargetDisplay = "label", visualization: FeatureListVisualizationType = "badge", threshold: float = -2, threshold_badge: float = 0, threshold_omit: float = -1, interactive: bool = True) -> None:
     next(initjs())
     eid = "vis-" + str(random())
     display(HTML("<div id='{id}'>{err_msg}</div>".format(id=eid, err_msg=html_err_msg())))
     display(Javascript("""
     if (window.argueview) {{
         window.ReactDom.render(
-            window.React.createElement(window.argueview.FeatureListVisualizer, {{explanation: {data}, framing: "{f}", lct: "{l}", threshold: {t}, thresholdBadge: {t2}, thresholdOmit: {t3}, visualization: "{vis}"}}),
+            window.React.createElement(window.argueview.FeatureListVisualizer, {{explanation: {data}, framing: "{f}", lct: "{l}", threshold: {t}, thresholdBadge: {t2}, thresholdOmit: {t3}, visualization: "{vis}", interactive: {i}}}),
             document.getElementById('{id}')
         );
     }}
-    """.format(id=eid, data=explanation.serialize(), f=framing, l=lct, t=threshold, t2=threshold_badge, t3=threshold_omit, vis=visualization)))
+    """.format(id=eid, data=explanation.serialize(), f=framing, l=lct, t=threshold, t2=threshold_badge, t3=threshold_omit, vis=visualization, i="true" if interactive else "false")))
